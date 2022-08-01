8af62b3ba1e17edd0e5d50ca184af7f5280c9d28
Бевзюк Максим Геннадьевич <Maksim.Bevzuk@avk.company>
2022-08-01 18:50:59 +0300

    hw3 done

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+//1
+
+let Number = 8;
+console.log(Number)
+
+let String = "Текст текст";
+console.log(String)
+
+let Boolean = false;
+console.log(Boolean)
+
+let Null = 88;
+console.log(Null)
+
+let symbol = Symbol("id");
+console.log(symbol)
+
+let bigint = 1880000000n;
+console.log(bigint)
+
+let my0bj = {
+    name: "Oleg",
+    lastName: "Ivanov",
+    age: 88
+};
+console.log(my0bj);
+
+
+//2
+
+var Variable = 99;
+console.log(Variable);
+
+var Variable = "Faeton";
+console.log(Variable);
+
+//4
+let strPlusNum = "turbo" + 55;
+console.log(strPlusNum)
+
+let strPlusBigint = "turbo" + 1880000;
+console.log(strPlusBigint)
+
+let strPlusSym = "turbo" + ("Mersedes") ;
+console.log(strPlusSym)
+
+
+let strPlusBool = "aqua" + false;
+console.log(strPlusBool)
+
+let strPlusNull = "turbo" + 88;
+console.log(strPlusNull)
+
+let NumPlusBigint = 8 + 180000;
+console.log(NumPlusBigint)
+
